67d457f463f23a095d0fb17722a517221edf1715
Fernando Maldonado <f_maldonado6@hotmail.com>
2021-08-22 18:47:34 -0500

    Fixed bugs

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 let matrixWidth = 3;
 let matrixHeight = 3;
 const matrix = [
-  [1, 1, 2,6],
-  [0, 2, 1,4],
-  [2, 1, 1,7],
+  [1, 1, 2, 6],
+  [0, 2, 1, 4],
+  [2, 1, 1, 7],
 ];
 
+function solveMatrix(matrixParam) {
 
-function solveMatrix() {
   for (let i = 0; i < matrixWidth; i++) {
-
     for (let j = 0; j < matrixHeight; j++) {
       if (i == j) continue;
 
-      let temp = matrix[j][i] / matrix[i][i];
+      let temp = matrixParam[j][i] / matrixParam[i][i];
 
       for (let k = 0; k <= matrixWidth; k++) {
-        matrix[j][k] -= matrix[i][k] * temp;
+        matrixParam[j][k] -= matrixParam[i][k] * temp;
       }
     }
   }
+  return matrixParam;
 }
 
-console.log(matrix);
-solveMatrix();
-console.log(matrix);
+console.log(JSON.parse(JSON.stringify(matrix)));
+
+console.log(solveMatrix(matrix));
