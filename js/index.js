45574932a4ef10c8f9400baff394aff628aa4dff
Fernando Maldonado <f_maldonado6@hotmail.com>
2021-08-23 18:31:07 -0500

    Added ui

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+const widthInput = document.getElementById("width");
+const heightInput = document.getElementById("height");
+const matrixContainer = document.querySelector(".matrix-container");
+
 let matrixWidth = 3;
 let matrixHeight = 3;
 const matrix = [
@@ -6,49 +10,62 @@ const matrix = [
   [1, 1, 0, 7],
 ];
 
+function generateMatrix() {
 
+  matrixWidth = Number.parseFloat(widthInput.value);
+  matrixHeight = Number.parseFloat(heightInput.value);
 
-function validateMatrix(matrixParam){
-  
+  matrixContainer.innerHTML = "";
+
+  for (let i = 0; i < matrixHeight + 1; i++) {
+    const div = document.createElement("div");
+    div.classList.add("row-container");
+
+    for (let j = 0; j < matrixWidth + 1; j++) {
+      const inputContainer = document.createElement("div");
+      inputContainer.classList.add("input-container");
+      const input = document.createElement("input");
+      input.type = "number"
+      inputContainer.appendChild(input);
+      div.appendChild(inputContainer);
+    }
+    matrixContainer.appendChild(div);
+  }
+}
+
+function validateMatrix(matrixParam) {
   //for para hacerle n pasadas a la matriz (indispensable)
-  for (let k = 0; k<matrixHeight; k++){
-    for(let i = 0; i < matrixWidth; i++){
+  for (let k = 0; k < matrixHeight; k++) {
+    for (let i = 0; i < matrixWidth; i++) {
       //verifica si el pivote es distinto de cero
-      if(matrixParam[i][i]!=0)
-        continue;
-      for(let j = 0; j < matrixHeight; j++){
-        
+      if (matrixParam[i][i] != 0) continue;
+      for (let j = 0; j < matrixHeight; j++) {
         //si la fila tiene algo distinto a cero en la posicion que estamos buscando
         //intercambia las filas
-        if(matrixParam[j][i]!=0 ){
+        if (matrixParam[j][i] != 0) {
           let temp = matrixParam[i];
           matrixParam[i] = matrixParam[j];
           matrixParam[j] = temp;
           break;
         }
-        
       }
-      
     }
   }
-  for(let i = 0;i<matrixHeight; i++){
-    if(matrixParam[i][i]==0){
+  for (let i = 0; i < matrixHeight; i++) {
+    if (matrixParam[i][i] == 0) {
       console.log("Matriz inconsistente");
       return null;
     }
   }
-    return matrixParam;
-  }
-
+  return matrixParam;
+}
 
 function solveMatrix(matrixParam) {
-  if (!matrixParam)
-    return null;
+  if (!matrixParam) return null;
   for (let i = 0; i < matrixWidth; i++) {
     for (let j = 0; j < matrixHeight; j++) {
       if (i == j) continue;
-      
-      
+
       let temp = matrixParam[j][i] / matrixParam[i][i];
 
       for (let k = 0; k <= matrixWidth; k++) {
@@ -61,7 +78,7 @@ function solveMatrix(matrixParam) {
 
 console.log(JSON.parse(JSON.stringify(matrix)));
 
-console.log(validateMatrix(matrix));
+console.log(validateMatrix(JSON.parse(JSON.stringify(matrix))));
 //si comentas la linea de abajo, la de arriba imprime el arreglo ordenado
 //si la dejas asi lo imprime resuelto no se pq
 console.log(solveMatrix(validateMatrix(matrix)));
