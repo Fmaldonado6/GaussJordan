9f266aba2ddbffe93f1aec9e7020f0d87f3d6eb8
Kevin Villalobos <kevin.villalobosvl@outlook.com>
2021-08-23 14:06:39 -0500

    Validacion de diagonal distinta de cero

--- js/index.js
+++ js/index.js
@@ -1,17 +1,54 @@
 let matrixWidth = 3;
 let matrixHeight = 3;
 const matrix = [
-  [1, 1, 2, 6],
-  [0, 2, 1, 4],
-  [2, 1, 1, 7],
+  [0, 3, 0, 6],
+  [2, 4, 1, 4],
+  [1, 1, 0, 7],
 ];
 
-function solveMatrix(matrixParam) {
 
+
+function validateMatrix(matrixParam){
+  
+  //for para hacerle n pasadas a la matriz (indispensable)
+  for (let k = 0; k<matrixHeight; k++){
+    for(let i = 0; i < matrixWidth; i++){
+      //verifica si el pivote es distinto de cero
+      if(matrixParam[i][i]!=0)
+        continue;
+      for(let j = 0; j < matrixHeight; j++){
+        
+        //si la fila tiene algo distinto a cero en la posicion que estamos buscando
+        //intercambia las filas
+        if(matrixParam[j][i]!=0 ){
+          let temp = matrixParam[i];
+          matrixParam[i] = matrixParam[j];
+          matrixParam[j] = temp;
+          break;
+        }
+        
+      }
+      
+    }
+  }
+  for(let i = 0;i<matrixHeight; i++){
+    if(matrixParam[i][i]==0){
+      console.log("Matriz inconsistente");
+      return null;
+    }
+  }
+    return matrixParam;
+  }
+
+
+function solveMatrix(matrixParam) {
+  if (!matrixParam)
+    return null;
   for (let i = 0; i < matrixWidth; i++) {
     for (let j = 0; j < matrixHeight; j++) {
       if (i == j) continue;
-
+      
+      
       let temp = matrixParam[j][i] / matrixParam[i][i];
 
       for (let k = 0; k <= matrixWidth; k++) {
@@ -24,4 +61,7 @@ function solveMatrix(matrixParam) {
 
 console.log(JSON.parse(JSON.stringify(matrix)));
 
-console.log(solveMatrix(matrix));
+console.log(validateMatrix(matrix));
+//si comentas la linea de abajo, la de arriba imprime el arreglo ordenado
+//si la dejas asi lo imprime resuelto no se pq
+console.log(solveMatrix(validateMatrix(matrix)));
