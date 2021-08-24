07129324c5b070ef9fc03b24868209a62b29a11d
Fernando Maldonado <f_maldonado6@hotmail.com>
2021-08-23 23:17:05 -0500

    Added gui

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 const widthInput = document.getElementById("width");
 const heightInput = document.getElementById("height");
 const matrixContainer = document.querySelector(".matrix-container");
+const snackbar = document.querySelector("#snackbar");
 
 let matrixWidth = 3;
 let matrixHeight = 3;
-const matrix = [
-  [10, 12, 2, 6],
-  [5, 6, 1, 3],
-  [2, 4, 4, 4],
-];
 
 function generateMatrix() {
   matrixWidth = Number.parseFloat(widthInput.value);
@@ -16,7 +12,7 @@ function generateMatrix() {
 
   matrixContainer.innerHTML = "";
 
-  for (let i = 0; i < matrixHeight + 1; i++) {
+  for (let i = 0; i < matrixHeight; i++) {
     const div = document.createElement("div");
     div.classList.add("row-container");
 
@@ -33,39 +29,47 @@ function generateMatrix() {
 }
 
 function validateMatrix(matrixParam) {
-  let val; //booleano que indica si uno de los terminos es linealmente independiente del mismo
-  //en la fila con la que se le compara
-  for (let i = 0; i < matrixWidth; i++) {
-    //for que controla a la fila estatica en cada comparacion
-    val = true; //se inicia en true para despues pasar a false si hay alguno que sea independiente
-    //con uno que sea independiente es suficiente para que sean validas las ecuaciones
-    for (let j = 0; j < matrixWidth; j++) {
-      //for que controla la iteracion de la fila con la que se compara la fila de i
-      if (i == j) continue;
-      for (let k = 0; k < matrixWidth + 1; k++) {
-        //for que controla la iteracion de las columnas (terminos) que se comparan
-        if (i == j) continue;
-        if (matrixParam[i][k] % matrixParam[j][k] != 0) {
-          //si uno de los terminos es independiente, se salta a la siguiente fila para comparar
-          if (k == matrixWidth) {
-            // este for de k va hasta el vector de soluciones, si todas los demas terminos fueron independientes, pero el ultimo no, entonces es inconsistente
-            console.log("El sistema es inconsistente");
-            return null;
-          }
-          //si no es el ultimo termino simplemente se salta a la siguiente fila para comparar
-          val = false;
-          break;
-        }
-      }
-      if (val) {
-        //si el val es true, no hubo ningun termino independiente en la comparacion por lo que es de soluciones infinitas
+  // let val; //booleano que indica si uno de los terminos es linealmente independiente del mismo
+  // //en la fila con la que se le compara
+  // for (let i = 0; i < matrixWidth; i++) {
+  //   //for que controla a la fila estatica en cada comparacion
+  //   val = true; //se inicia en true para despues pasar a false si hay alguno que sea independiente
+  //   //con uno que sea independiente es suficiente para que sean validas las ecuaciones
+  //   for (let j = 0; j < matrixWidth; j++) {
+  //     //for que controla la iteracion de la fila con la que se compara la fila de i
+  //     if (i == j) continue;
+  //     for (let k = 0; k < matrixWidth + 1; k++) {
+  //       //for que controla la iteracion de las columnas (terminos) que se comparan
+  //       if (i == j) continue;
+  //       if (matrixParam[i][k] % matrixParam[j][k] != 0) {
+  //         //si uno de los terminos es independiente, se salta a la siguiente fila para comparar
+  //         if (k == matrixWidth) {
+  //           // este for de k va hasta el vector de soluciones, si todas los demas terminos fueron independientes, pero el ultimo no, entonces es inconsistente
+  //           console.log("El sistema es inconsistente");
+  //           return null;
+  //         }
+  //         //si no es el ultimo termino simplemente se salta a la siguiente fila para comparar
+  //         val = false;
+  //         break;
+  //       }
+  //     }
+  //     if (val) {
+  //       //si el val es true, no hubo ningun termino independiente en la comparacion por lo que es de soluciones infinitas
 
-        console.log(
-          "Dos o mas filas son linealmente dependientes por lo que el sistema tiene soluciones infinitas"
-        );
-        return null;
-      }
-    }
+  //       console.log(
+  //         "Dos o mas filas son linealmente dependientes por lo que el sistema tiene soluciones infinitas"
+  //       );
+  //       return null;
+  //     }
+  //   }
+  // }
+
+  const values = [];
+
+  for (let row of matrixParam) {
+    const sum = row.reduce((x, y) => x + y);
+    if (values.find((x) => sum % x == 0 || x % sum == 0)) return null;
+    values.push(sum);
   }
 
   //for para hacerle k pasadas a la matriz (indispensable) dificil de explicar en texto xd
@@ -85,12 +89,7 @@ function validateMatrix(matrixParam) {
       }
     }
   }
-  for (let i = 0; i < matrixHeight; i++) {
-    if (matrixParam[i][i] == 0) {
-      console.log("Matriz inconsistente, tiene ceros en la diagonal principal");
-      return null;
-    }
-  }
+
   return matrixParam;
 }
 
@@ -103,6 +102,8 @@ function solveMatrix(matrixParam) {
 
       let temp = matrixParam[j][i] / matrixParam[i][i];
 
+      if (Number.isNaN(temp) || !Number.isFinite(temp)) return null;
+
       for (let k = 0; k <= matrixWidth; k++) {
         matrixParam[j][k] -= matrixParam[i][k] * temp;
       }
@@ -111,9 +112,38 @@ function solveMatrix(matrixParam) {
   return matrixParam;
 }
 
-console.log(JSON.parse(JSON.stringify(matrix)));
+function calculate() {
+  let matrix = [];
 
-console.log(validateMatrix(JSON.parse(JSON.stringify(matrix))));
-//si comentas la linea de abajo, la de arriba imprime el arreglo ordenado
-//si la dejas asi lo imprime resuelto no se pq
-console.log(solveMatrix(validateMatrix(matrix)));
+  for (let col of matrixContainer.children) {
+    const row = [];
+    for (let element of col.children) {
+      const input = element.firstChild;
+      const value = Number.parseFloat(input.value);
+      if (Number.isNaN(value))
+        return showSnackbar("No puede haber valores en blanco");
+      row.push(value);
+    }
+    matrix.push(row);
+  }
+
+  matrix = validateMatrix(matrix);
+
+  if (!matrix)
+    return showSnackbar("La matriz tiene elementos linealmente dependientes");
+
+  matrix = solveMatrix(matrix);
+
+  if (!matrix)
+    return showSnackbar("La matriz es inconsistente");
+}
+
+function showSnackbar(message) {
+  snackbar.classList.add("show");
+  snackbar.innerHTML = message;
+  setTimeout(function () {
+    snackbar.classList.remove("show");
+  }, 2750);
+}
+
+generateMatrix();
